36cd225a834bd6fb130a992a4f8e31e3bfdffcd7
sh1mc <sh1@coins.tsukuba.ac.jp>
2022-09-04 21:56:39 +0900

    add addition and subtraction

--- sh1mcc.c
+++ sh1mcc.c
@@ -11,7 +11,25 @@ int main(int argc, char* argv[])
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
+
+    char* p = argv[1];
+
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "Unexpected input: '%c'", *p);
+        exit(EXIT_FAILURE);
+    }
     printf("\tret\n");
     return 0;
 }
